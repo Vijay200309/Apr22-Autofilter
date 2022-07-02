10ba4fdcb923b27b3c7468731d8afd47592c657c
Vijayraj patil <90549591+Vijay200309@users.noreply.github.com>
2022-07-02 19:43:52 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -41,7 +41,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("এটা তোমার জন্য নয় সোনা👀", show_alert=True)
+        return await query.answer("This Is Not For Uhh 👀", show_alert=True)
     try:
         offset = int(offset)
     except:
